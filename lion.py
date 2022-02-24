41c30134620844eabf068ab7b4938d50a8868141
shaikh <shaikhes@97gmail.com>
2022-02-24 13:18:13 +0530

    new add two paragraf

--- lion.py
+++ lion.py
@@ -3,7 +3,12 @@ I hide and I sneak.
 I won’t make a noise.
 My shoes do not squeak.
 
-I’m being as quiet
-as I can be
-but my lion roars
-at poetry.
+If he’s near short fiction
+he bats with his paws.
+If he sees an atlas
+it goes in his jaws.
+
+He’s usually calm
+like very old dogs
+He just gets excited near
+card catalogs..
